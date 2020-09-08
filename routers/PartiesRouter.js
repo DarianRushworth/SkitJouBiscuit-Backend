6dc7ecae60ddbbd2546fead757bd53b185d0dde8
DarianRushworth <darianerrolrushworth@gmail.com>
2020-09-08 15:30:14 +0200

    Specific Party Router sending correct data

--- routers/PartiesRouter.js
+++ routers/PartiesRouter.js
@@ -5,6 +5,29 @@ const Party = require("../models").party
 const router = new Router()
 
 router.get(
+    "/:id",
+    async(req, res, next) => {
+        const partyIdNeeded = parseInt(req.params.id)
+        if(!partyIdNeeded){
+            res.status(400).send("Oops the url malfunctioned, go back refresh and try again.")
+        }
+
+        try{
+            const specificParty = await Party.findByPk(partyIdNeeded)
+
+            if(!specificParty){
+                res.status(404).send("This party has gone on a trip... go back and try again.")
+            } else{
+                res.status(202).send(specificParty)
+            }
+
+        } catch(error){
+            next(error)
+        }
+    }
+)
+
+router.get(
     "/list",
     async(req, res, next) => {
         const limit = req.query.limit || 2
